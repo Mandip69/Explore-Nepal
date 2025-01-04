84adb9ed0fc592fcd4d7e19f13503e7dbc6a373c
mandip chaudhary <mandip.chaudhary2022@pcmgmt.edu.np>
2025-01-04 14:01:07 +0545

    home page done

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,25 @@
 import React from "react";
 import destinations from "../data/destinations.json";
 import DestinationCard from "../components/DestinationsCard";
+// import Nepal from "../images/Nepal.jpeg";
 
 const Home = () => {
   return (
     <div>
       <header
         className="hero-section bg-cover bg-center h-96"
-        style={{ backgroundImage: "url('/images/hero-image.jpg')" }}
+        style={{ backgroundImage: `url('/images/Nep.png')` }}
       >
-        <div className="bg-black bg-opacity-50 h-full flex items-center justify-center">
-          <h1 className="text-white text-4xl font-bold">Welcome to Nepal</h1>
-        </div>
+   <div className="bg-black bg-opacity-50 h-full flex flex-col items-center justify-center text-center">
+      <h1 className="text-white text-3xl font-bold mb-4">
+                Welcome to Nepal
+             </h1>
+           <p className="text-white text-base">
+            A land of breathtaking landscapes and cultural heritage.
+             </p>
+            </div>
+
+
       </header>
       <section className="container mx-auto mt-8">
         <h2 className="text-2xl font-bold mb-4">Top Destinations</h2>
